2a50af1398b2fa91c29c699c11bc1e093323fb81
AlbertoGruning-Fedora <alberto.gruning.zen@gmail.com>
2021-09-14 20:07:02 -0400

    cap12-component

---
+++ src/screens/ComponentScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Text, StyleSheet} from 'react-native'
+
+const componentScreen =()=>{
+    return <Text style={styles.textStyle}>This is the component Screen</Text>
+}
+const styles = StyleSheet.create({
+    textStyle: {
+        fontSize:30
+    }
+})
+
+export default ComponentScreen
