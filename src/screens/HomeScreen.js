7310d3c8f4dd399fbd39d61b00aac87465199fa3
vi-dell-fedora <kimono76.live@gmail.com>
2022-01-24 22:16:00 -0300

    feat:image

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,7 +2,13 @@ import React from 'react';
 import { Text, StyleSheet } from 'react-native';
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+return <img 
+          alt="logo" 
+          src={require('./monkey.svg')} 
+          style={{ height: "70px", paddingRight: "10px" }} 
+        /> 
+
+//  return <Text style={styles.text}>Hola wendy: esto es una aplicacion de prueba para QIMONO</Text>;
 };
 
 const styles = StyleSheet.create({
