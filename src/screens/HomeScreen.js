22e66d049d0b3b1dcc19d50badebea0d621a5751
vi-dell-fedora <kimono76.live@gmail.com>
2022-02-21 21:53:50 -0300

    feat:jsx elements #cap15 done

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,27 +1,36 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 
 const HomeScreen = () => {
+  const javaScriptVariable = ' Welcome home dear friend!';
+  
+  const jsxInAaVariable = <Text> by monkeys </Text>
 
 var imageName = require('./monkey.svg')
+  //style={styles.greetingText}
 
-//return <div>
-//	<img 
-//          alt="logo"
-//          src={imageName} 
-//          style={{ height: "70px", paddingRight: "10px" }} 
-//        />
-//	</div>
+  return <View>
+    <Text style={styles.greetingText}>{javaScriptVariable}</Text>
+    <Text style={styles.text}>Qi MONO</Text>
+    <Text> Developed for monkeys</Text>
+    {jsxInAaVariable}
+  </View>
 
-  return <Text style={styles.text}>Qi MONO</Text>;
-};
+}
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 50,
     color: 'navy',
     fontFamily: 'sans-serif-light',
+    // fontWeight: 'bold', 
   },
+  greetingText:{
+    //align: 'right',
+    //alignContent: 'center',
+    color: 'navy',
+    fontWeight: 'bold',
+  }
 });
 
 export default HomeScreen;
