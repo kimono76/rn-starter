9b50d018ebd88116cba52e419e9333d39debfd4d
qimono-desk-fedora <kimono76.live@gmail.com>
2022-03-20 12:27:33 -0300

    feat:list component#cap19

--- App.js
+++ App.js
@@ -2,13 +2,16 @@ import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack"
 import HomeScreen from "./src/screens/HomeScreen"
 import Exercise17Screen from "./src/screens/Exercise17Screen"
+import ListScreen from "./src/screens/ListScreen"
 
 const navigator = createStackNavigator(
   {
-    Home: Exercise17Screen,
+    Home: HomeScreen,
+    Exercise17: Exercise17Screen,
+    List: ListScreen,
   },
   {
-    initialRouteName: "Home",
+    initialRouteName: "List",
     defaultNavigationOptions: {
       title: "App",
     },
--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,2 +1,9 @@
-import react from 'react'
+import React from 'react'
 import { Text, View, StyleSheet } from 'react-native'
+
+const ListScreen = () => {
+    return(
+        <Text>List Screen</Text>
+    )
+}
+export default ListScreen
