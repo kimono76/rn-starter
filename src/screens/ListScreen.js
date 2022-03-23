bdff8fefc22c3cdc26b6b1c6fefaa5cbb04edff1
vi-ins <kimono76.live@gmail.com>
2022-03-22 22:04:10 -0300

    feat:listItem#cap23

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,9 +1,31 @@
 import React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet,FlatList } from 'react-native'
 
 const ListScreen = () => {
+    const arrayOfObjects=[
+        {name:'Arduino Uno'},
+        {name:'Raspberry Pi 3'},
+        {name:'Arduino Pro Mini'},
+        {name:'Raspberry Pi Pico'},
+        {name:'Raspberry Pi 4'},
+    ] ;
+    const arrayOfObjectsWithKeys=[
+        {name:'Arduino Uno'         ,key:'1'},
+        {name:'Raspberry Pi 3'      ,key:'2'},
+        {name:'Arduino Pro Mini'    ,key:'3'},
+        {name:'Raspberry Pi Pico'   ,key:'4'},,
+        {name:'Raspberry Pi 4'      ,key:'5'},
+    ] ;
+
     return(
-        <Text>List Screen</Text>
+        <FlatList 
+            keyExtractor={ item => item.name }
+            data={ arrayOfObjects }
+            renderItem ={ ({ item }) =>{
+                return <Text>{item.name}</Text>
+                }
+            } 
+        />
     )
 }
 export default ListScreen
